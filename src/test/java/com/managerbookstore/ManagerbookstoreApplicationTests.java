9400ed323c7735a38ee99ebbcfcf18c9468babda
Praaja shettigar <praajnashettigar123@gmail.com>
2024-07-30 21:50:27 +0530

    Initial setup with required dependencies

---
+++ src/test/java/com/managerbookstore/ManagerbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managerbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerbookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
